22791f25ad7873d95d83d9f9cf7b13818a4c6f2a
meghanasvd30@gmail.com <meghanasvd30@gmail.com>
2025-07-16 09:57:31 +0000

    thi

--- resource.tf
+++ resource.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "storeterra" {
  
 resource "azurerm_storage_account" "saterra" {
 
-    name                     = "terrastoragesou"
+    name                     = "terrastoragesmeghana"
 
     resource_group_name      = azurerm_resource_group.storeterra.name
 
